2cd2c4a48983289af85af452a80a1987688a2699
mdomzol <domzolmichal@gmail.com>
2026-06-06 13:02:09 +0200

    Utworzenie sztucznych zgłoszeń

---
+++ TicketSystem/DataSeeder/FakeDataSeeder.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TicketSystem.Models;
+using TicketSystem.Services;
+
+namespace TicketSystem.DataSeeder;
+
+public static class FakeDataSeeder
+{
+    public static void Seed(TicketService service)
+    {
+        var tickets = new List<Ticket>
+        {
+            new BugTicket("Blad logowania", "System crashuje po wpisaniu hasla"),
+            new FeatureRequestTicket("Tryb ciemny", "Dodanie dark mode"),
+            new TechnicalTicket("Wolne dzialanie", "Aplikacja laguje przy duzej liczbie ticketow")
+        };
+
+        foreach (var ticket in tickets)
+        {
+            service.CreateTicket(ticket);
+        }
+    }
+}
