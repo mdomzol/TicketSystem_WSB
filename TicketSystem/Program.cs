26a5bab1d93447d6b9ce071a98715a8e89b52975
mdomzol <domzolmichal@gmail.com>
2026-06-06 12:34:37 +0200

    Dodanie kodu tworzącego wstępne zgłoszenie

--- TicketSystem/Program.cs
+++ TicketSystem/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using TicketSystem.Models;
+using TicketSystem.Repositories;
+using TicketSystem.Services;
+
+var repository = new InMemoryTicketRepository();
+var service = new TicketService(repository);
+
+var ticket = new BugTicket("Blad logowania", "Uzytkownik nie moze się zalogowac.");
+
+service.CreateTicket(ticket);
+
+foreach(var t in service.GetAllTickets())
+{
+    Console.WriteLine($"{t.Id} | {t.Title} | {t.Status}");
+}
